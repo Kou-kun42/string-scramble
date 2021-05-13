8889e821ddf9952766883811eaacd64e50d84f14
Kou-kun42 <shinigamithekido@gmail.com>
2021-05-13 10:38:10 -0400

    Changed name

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 /**
- * Randomly scrambles an input string.
+ * Returns a randomly scrambled input string.
  *
  * @param {String} string
  * @return {String}
  */
 
-exports.stringScrambler = (str) => {
+exports.scramble = (str) => {
   const strArr = str.split("");
   const len = strArr.length;
-
   for (var i = 0; i < len - 1; i++) {
     const j = Math.round(Math.random() * (len - 1));
     const hold = strArr[i];
@@ -17,7 +16,3 @@ exports.stringScrambler = (str) => {
   }
   return strArr.join("");
 };
-
-let s = "ABC DE F";
-
-console.log(this.stringScrambler(s));
